0c78ff99271733e3b2bfb414dff9008002bae41c
Mahitha A <mahitha@everest.engineering>
2023-01-18 14:45:10 +0530

    Display user details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router';
+import { PrismaClient } from "@prisma/client";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+function Home({users}: InferGetServerSidePropsType<typeof getServerSideProps>) {
     const router = useRouter()
 
     return (
@@ -10,10 +12,41 @@ export default function Home() {
                 <title>Blogs</title>
             </Head>
             <main>
-                <button type="button" onClick={() => router.push('/user/new')}>
-                    Add User
-                </button>
+                <>
+                    <button type="button" onClick={() => router.push('/user/new')}>
+                        Add User
+                    </button>
+
+                    <div> All users</div>
+                    {
+                        users.map(user => (
+                            <div key={user.id}>
+                                <div>
+                                    <span>Email:</span>
+                                    <span>{user.email}</span>
+                                </div>
+                                <div>
+                                    <span>First Name:</span>
+                                    <span>{user.firstName}</span>
+                                </div>
+                                <div>
+                                    <span>Last Name:</span>
+                                    <span>{user.lastName}</span>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </>
             </main>
         </>
     )
 }
+
+export async function getServerSideProps() {
+    const prisma = new PrismaClient()
+    const users = await prisma.user.findMany()
+
+    return {props: {users}}
+}
+
+export default Home
