5f18699d941be8a3ddb9f6485b7abe84d1eb2568
Mahitha A <mahitha@everest.engineering>
2023-01-23 16:00:19 +0530

    Change to state

--- src/pages/user/new.tsx
+++ src/pages/user/new.tsx
@@ -1,16 +1,21 @@
 import {trpc} from "../../utils/trpc";
 import {useRouter} from "next/router";
+import {useState} from "react";
 
 export default function NewUser() {
     const router = useRouter()
     const addUserMutation = trpc.user.add.useMutation()
+
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         // @ts-ignore
         addUserMutation.mutate({
-            email: event.target["email"].value,
-            firstName: event.target["firstname"].value,
-            lastName: event.target["lastname"].value
+            email: email,
+            firstName: firstName,
+            lastName: lastName
         });
     }
 
@@ -29,13 +34,13 @@ export default function NewUser() {
     return (
         <form onSubmit={(event) => handleSubmit(event)}>
             <label htmlFor="firstname">First Name</label>
-            <input type="text" id="firstname" name="firstname" required />
+            <input type="text" id="firstname" name="firstname" required onChange={(e) => setFirstName(e.target.value)}/>
 
             <label htmlFor="lastname">Last Name</label>
-            <input type="text" id="lastname" name="lastname" required />
+            <input type="text" id="lastname" name="lastname" required onChange={(e) => setLastName(e.target.value)}/>
 
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email" required />
+            <input type="text" id="email" name="email" required onChange={(e) => setEmail(e.target.value)}/>
 
             <button type="submit">Submit</button>
         </form>
